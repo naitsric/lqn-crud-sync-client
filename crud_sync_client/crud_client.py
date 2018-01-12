2dd6ae10e33c529e050738d2f0d2670ffd2c68d2
Cristian Duque <dev.cristian.duque@gmail.com>
2018-01-12 13:51:08 -0500

    add find one concept

--- crud_sync_client/crud_client.py
+++ crud_sync_client/crud_client.py
@@ -1,14 +1,14 @@
 from nameko.standalone.rpc import ClusterRpcProxy
 
 
-class LQNConceptsGraphQClient:
+class LQNGraphQClient:
     def __init__(self, amqp_user, amqp_password, amqp_host, amqp_virtual_host):
         self.config = {
             "AMQP_URI": "amqp://{}:{}@{}/{}".format(amqp_user, amqp_password, amqp_host, amqp_virtual_host)
         }
 
-    def get_user_id_remax(self):
+    def find_one_concept_by_code(self, code):
         with ClusterRpcProxy(self.config) as rpc:
-            find_remax_default_user = rpc.crud.find_remax_default_user()
-            user_id = find_remax_default_user['data']['allUsers'][0]["id"]
+            find_one_concept_by_code = rpc.crud.find_one_concept_by_code(code)
+            user_id = find_one_concept_by_code['data']['allPaymentConcepts'][0]["id"]
             return user_id
