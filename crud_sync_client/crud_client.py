81a38a212053c7642ffa2d73bd5e5309b55ace82
Cristian Duque <dev.cristian.duque@gmail.com>
2018-01-12 18:40:43 -0500

    create token

--- crud_sync_client/crud_client.py
+++ crud_sync_client/crud_client.py
@@ -21,3 +21,8 @@ class LQNGraphQClient:
         with ClusterRpcProxy(self.config) as rpc:
             find_one_concept_by_code = rpc.crud.update_necessity_concept_send_to_pay(id)
             return find_one_concept_by_code['data']['updateNecessityConcept']
+
+    def update_user_token(self, user_id, token):
+        with ClusterRpcProxy(self.config) as rpc:
+            update_user_token = rpc.crud.update_user_token(user_id, token)
+            return update_user_token['data']['updateUser']
